1a92e3aad606560b26ec946fbfce8009de3c84ec
paragon0107 <smg0725@naver.com>
2025-05-12 15:10:45 +0900

    feat:ignore 설정

--- src/main/java/school/fingerprint/patient/repository/entity/Patient.java
+++ src/main/java/school/fingerprint/patient/repository/entity/Patient.java
@@ -2,17 +2,22 @@ package school.fingerprint.patient.repository.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
 @Entity
-@RequiredArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
 @Getter
 public class Patient {
+
     @Id
     private Long id;
     private String name;
