8252c9f0d97baccdc7d5f7ec73329538c264463a
paragon0107 <smg0725@naver.com>
2025-06-14 10:49:20 +0900

    feat: qwer

--- src/main/java/school/fingerprint/patient/service/PatientService.java
+++ src/main/java/school/fingerprint/patient/service/PatientService.java
@@ -44,9 +44,10 @@ public class PatientService {
                 location.predicted_location()
         );
         locationHandler.updatePatientLocatedInfo(patientLocatedInfo);
+        System.out.println("환자 이름: " + patient.getName());
         System.out.println("환자 ssid : " + ssid);
         System.out.println("업데이트 시간" + new Date());
-        System.out.println("환자 위치 : " + patientLocatedInfo.getPlace());
+        System.out.println("환자 위치 : " + location);
         System.out.println("환자 위치 층: " + patientLocatedInfo.getFloor());
         System.out.println(request.toString());
     }
